f642b6790386a383d8998746ee26ac513f32662a
tape <a5810032@gmail.com>
2018-10-15 19:38:48 +0700

    first

--- soapClient.py
+++ soapClient.py
@@ -4,18 +4,26 @@ import xmlschema
 
 
 client = Client('http://localhost:8000/soapAPI1/?wsdl')
-result = client.service.listfoodService()
-# resultDB = client.service.test()
-
+resultlistfood = client.service.listfoodService()
 resultXMLtest = client.service.getGrapXMLdata()
 # print(resultXMLtest)
 
-utf8_parser = etree.XMLParser(encoding='utf-8')
-root = etree.fromstring(resultXMLtest.encode('utf-8'), parser=utf8_parser)
-# print(root)
-for food in root.findall('item'):
-    print("date :",food.find('date_use').text)
-    print("x :",food.find('x_lat').text)
-    print("y :",food.find('y_value').text)
+root = etree.fromstring(resultlistfood)
+for food in root.findall('food'):
+    print("name :",food.find('name').text)
+    print("price :",food.find('price').text)
+    print("descr :",food.find('description').text.replace('\n',' '))
+    print("cal :",food.find('calories').text)
     print()
+
+# # print(resultXMLtest)
+
+# utf8_parser = etree.XMLParser(encoding='utf-8')
+# root = etree.fromstring(resultXMLtest.encode('utf-8'), parser=utf8_parser)
+# # print(root)
+# for food in root.findall('item'):
+#     print("date :",food.find('date_use').text)
+#     print("x :",food.find('x_lat').text)
+#     print("y :",food.find('y_value').text)
+#     print()
 
